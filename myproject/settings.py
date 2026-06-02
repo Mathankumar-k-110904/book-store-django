bcea458a6a99443159c02e21aee2de738966ead2
Mathan Kumar K <mathankumar3564@gmail.com>
2026-06-03 00:17:38 +0530

    Changed database to sqlite

--- myproject/settings.py
+++ myproject/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookstore',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
